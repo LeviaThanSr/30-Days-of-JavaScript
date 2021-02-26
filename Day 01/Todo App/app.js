74e4463fb94e23c7ccd53a7952f04a1ad9b8af84
LeviaThanSr <omarsarour@gmail.com>
2021-02-26 23:58:52 +0100

    Update App

---
+++ Day 01/Todo App/app.js
@@ -0,0 +1,61 @@
+const inputField = document.querySelector(".inputField input");
+const addTaskBtn = document.querySelector(".inputField button");
+const task = document.querySelector(".tasks");
+const deleteAllBtn = document.querySelector(".footer button");
+
+inputField.onkeyup = () => {
+  let inputText = inputField.value;
+  if (inputText.trim() != 0) {
+    addTaskBtn.classList.add("active");
+  } else {
+    addTaskBtn.classList.remove("active");
+  }
+};
+
+addTaskBtn.onclick = () => {
+  let inputText = inputField.value;
+  let getLocalStorage = localStorage.getItem("Add Todo");
+  if (getLocalStorage == null) {
+    todoArr = [];
+  } else {
+    todoArr = JSON.parse(getLocalStorage);
+  }
+  todoArr.push(inputText);
+  localStorage.setItem("Add Todo", JSON.stringify(todoArr));
+  showTask();
+};
+
+const showTask = () => {
+  let getLocalStorage = localStorage.getItem("Add Todo");
+  if (getLocalStorage == null) {
+    todoArr = [];
+  } else {
+    todoArr = JSON.parse(getLocalStorage);
+  }
+  const taskNum = document.querySelector(".taskNum");
+  taskNum.textContent = todoArr.length;
+  let newListItem = "";
+  todoArr.forEach((i, index) => {
+    newListItem += `<li>
+                      ${i}
+                      <button class="btn btn-success" onClick = "taskCompleted()" ><i class="fas fa-check"></i></button>
+                      <button class="btn btn-warning" onClick = "deleteTask(${index})" ><i class="fas fa-times"></i></button>
+                    </li>`;
+  });
+  task.innerHTML = newListItem;
+  inputField.value = "";
+};
+
+const deleteTask = (task) => {
+  let getLocalStorage = localStorage.getItem("Add Todo");
+  todoArr = JSON.parse(getLocalStorage);
+  todoArr.splice(task, 1);
+  localStorage.setItem("Add Todo", JSON.stringify(todoArr));
+  showTask();
+};
+
+deleteAllBtn.onclick = () => {
+  todoArr = [];
+  localStorage.setItem("Add Todo", JSON.stringify(todoArr));
+  showTask();
+};
