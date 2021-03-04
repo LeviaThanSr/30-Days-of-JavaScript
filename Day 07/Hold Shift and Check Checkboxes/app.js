5a1e8282dad470a5b1b2c344363ced74469e7226
LeviaThanSr <omarsarour@gmail.com>
2021-03-04 20:28:20 +0100

    Update App

--- Day 07/Hold Shift and Check Checkboxes/app.js
+++ Day 07/Hold Shift and Check Checkboxes/app.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+const checkBoxes = document.querySelectorAll(".inbox input[type='checkbox']");
+
+let lastChecked;
+function handleCheck(e) {
+  if (e.shiftKey && this.checked) {
+    let inBetween = false;
+    checkBoxes.forEach((checkBox) => {
+      if (checkBox === this || checkBox === lastChecked) {
+        inBetween = !inBetween;
+      }
+      if (inBetween) {
+        checkBox.checked = true;
+      }
+    });
+  }
+  lastChecked = this;
+}
+
+checkBoxes.forEach((checkBox) =>
+  checkBox.addEventListener("click", handleCheck)
+);
