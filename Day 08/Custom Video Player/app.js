e448de7eca1e91147ea1c76c3e09a66fc6f367f8
LeviaThanSr <omarsarour@gmail.com>
2021-03-05 20:31:18 +0100

    Update App

--- Day 08/Custom Video Player/app.js
+++ Day 08/Custom Video Player/app.js
@@ -1,1 +1,34 @@
-console.log("Hello World!");
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+function togglePlay() {
+  video[video.paused ? "play" : "pause"]();
+}
+
+function updateButton() {
+  toggle.innerHTML = this.paused
+    ? '<i class="fas fa-play"></i>'
+    : '<i class="fas fa-pause"></i>';
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRange() {
+  video[this.name] = this.value;
+}
+
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+
+toggle.addEventListener("click", togglePlay);
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+
+ranges.forEach((range) => range.addEventListener("change", handleRange));
+ranges.forEach((range) => range.addEventListener("mousemove", handleRange));
