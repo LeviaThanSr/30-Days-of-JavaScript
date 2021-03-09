22f43f818d573a9dd445cb65f61725c237eed8d7
LeviaThanSr <omarsarour@gmail.com>
2021-03-09 23:37:48 +0100

    Update App

--- Day 12/Click and Drag/app.js
+++ Day 12/Click and Drag/app.js
@@ -1,1 +1,34 @@
-console.log("Hello World!");
+const slider = document.querySelector(".items");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+function mouseDown(e) {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+}
+
+function mouseMove(e) {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2;
+  slider.scrollLeft = scrollLeft - walk;
+}
+
+function mouseUp() {
+  isDown = false;
+  slider.classList.remove("active");
+}
+
+function mouseLeave() {
+  isDown = false;
+  slider.classList.remove("active");
+}
+
+slider.addEventListener("mousedown", mouseDown);
+slider.addEventListener("mousemove", mouseMove);
+slider.addEventListener("mouseup", mouseUp);
+slider.addEventListener("mouseleave", mouseLeave);
