c27d3d717ad3da3a43996b3218d8608a20adc76b
LeviaThanSr <omarsarour@gmail.com>
2021-03-10 21:15:21 +0100

    Update App

--- Day 01/Todo App/app.js
+++ Day 01/Todo App/app.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+const clear = document.querySelector(".clear");
+const date = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+const addToDoBtn = document.getElementById("add");
+
+function addToDo(todo) {
+  const position = "beforeend";
+  const text = `<li class="item">
+  <i class="co far fa-circle" job="complete"></i><p class="text">${todo}</p><i class="delete far fa-trash-alt" job="delete"></i></li>`;
+
+  list.insertAdjacentHTML(position, text);
+}
+
+function handleEnter(e) {
+  if (e.keyCode == 13 || e.type == "click") {
+    const todo = input.value;
+    if (!todo) return;
+
+    addToDo(todo);
+    input.value = "";
+  }
+}
+
+document.addEventListener("keyup", handleEnter);
+addToDoBtn.addEventListener("click", handleEnter);
