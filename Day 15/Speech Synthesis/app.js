1f6a8e423296c92b8931ee20b73fd9660f9254b8
LeviaThanSr <omarsarour@gmail.com>
2021-03-12 23:00:53 +0100

    Update App

--- Day 15/Speech Synthesis/app.js
+++ Day 15/Speech Synthesis/app.js
@@ -1,1 +1,6 @@
-console.log("Hello World!");
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector("#speak");
+const stopButton = document.querySelector("#stop");
