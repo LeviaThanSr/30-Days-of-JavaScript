f46bc010c1b746576400db49ced2388e45be33ad
LeviaThanSr <omarsarour@gmail.com>
2021-03-16 21:23:23 +0100

    Update App

--- Day 18/Tip Calculator/app.js
+++ Day 18/Tip Calculator/app.js
@@ -1,3 +1,4 @@
+const input = document.querySelector(".input");
 const costInput = document.querySelector(".cost");
 const serviceInput = document.querySelector(".service");
 const peopleInput = document.querySelector(".people");
@@ -5,7 +6,7 @@ const calcBtn = document.querySelector("#calc-btn");
 const tip = document.querySelector(".tip-wrap .tip");
 
 function calculateTip(e) {
-  if (e.keyCode === 13) {
+  if (e.keyCode === 13 || e.type === "click") {
     const cost = Number(costInput.value);
     const service = Number(serviceInput.value) / 100;
     const people = Number(peopleInput.value);
@@ -17,5 +18,13 @@ function calculateTip(e) {
   }
 }
 
+function preventDefault(e) {
+  // console.log(e.which);
+  if ((e.which != 8 && e.which != 0 && e.which < 48) || e.which > 57) {
+    e.preventDefault();
+  }
+}
+
+input.addEventListener("keypress", preventDefault);
 calcBtn.addEventListener("click", calculateTip);
 document.addEventListener("keyup", calculateTip);
