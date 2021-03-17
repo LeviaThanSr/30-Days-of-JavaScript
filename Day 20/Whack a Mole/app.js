f3897b76fb6badb84119d731e3a3af2035a7518a
LeviaThanSr <omarsarour@gmail.com>
2021-03-17 20:31:56 +0100

    Update App

--- Day 20/Whack a Mole/app.js
+++ Day 20/Whack a Mole/app.js
@@ -1,1 +1,49 @@
-console.log("Hello World!");
+const holes = document.querySelectorAll(".hole");
+const scoreBoard = document.querySelector(".score");
+const moles = document.querySelectorAll(".mole");
+const startBtn = document.querySelector(".start");
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
+
+function peep() {
+  const time = randomTime(300, 1000);
+  const hole = randomHole(holes);
+
+  hole.classList.add("up");
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) peep();
+  }, time);
+}
+
+function startGame() {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  peep();
+  setTimeout(() => (timeUp = true), 10000);
+}
+
+function bonk(e) {
+  if (!e.isTrusted) return;
+  score++;
+  this.parentNode.classList.remove("up");
+  scoreBoard.textContent = score;
+}
+
+startBtn.addEventListener("click", startGame);
+moles.forEach((mole) => mole.addEventListener("click", bonk));
