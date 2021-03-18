b4dc7d4fe7e91260fdbf36e4d2aa0e39b2a30094
LeviaThanSr <omarsarour@gmail.com>
2021-03-18 22:48:53 +0100

    Update App

--- Day 21/Random Password Generator/app.js
+++ Day 21/Random Password Generator/app.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+const passwordOutput = document.getElementById("password");
+const GeneratePasswordBtn = document.getElementById("btn");
+
+function GeneratePassword(e) {
+  let characters =
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+?><:{}[]";
+  let password = "";
+  let passwordLength = 16;
+
+  for (let i = 0; i < passwordLength; i++) {
+    let randomNum = Math.floor(Math.random() * characters.length);
+    password += characters.substring(randomNum, randomNum + 1);
+  }
+
+  passwordOutput.value = password;
+
+  setTimeout(() => {
+    passwordOutput.value = "";
+  }, 5000);
+}
+
+GeneratePasswordBtn.addEventListener("click", GeneratePassword);
