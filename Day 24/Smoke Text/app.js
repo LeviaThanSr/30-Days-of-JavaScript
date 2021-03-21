3490c7a11d274b4a3ecd5c5478df1cf56427c61e
LeviaThanSr <omarsarour@gmail.com>
2021-03-21 21:20:39 +0100

    Update App

--- Day 24/Smoke Text/app.js
+++ Day 24/Smoke Text/app.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+const text = document.querySelector(".text");
+text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
+
+const letters = document.querySelectorAll(".text span");
+
+for (let i = 0; i < letters.length; i++) {
+  letters[i].addEventListener("mouseover", smokeEffect);
+}
+
+function smokeEffect() {
+  this.classList.add("active");
+}
