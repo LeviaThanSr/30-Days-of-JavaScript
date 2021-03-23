1dedbe74e993aad2ea15bb5d279e4b8018f87f0a
LeviaThanSr <omarsarour@gmail.com>
2021-03-23 21:27:57 +0100

    Update App

--- Day 26/Tabs/app.js
+++ Day 26/Tabs/app.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+const Tabs = document.querySelector(".tabs");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+
+function handleTabs(e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    btns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+
+    e.target.classList.add("active");
+    articles.forEach((article) => {
+      article.classList.remove("active");
+    });
+
+    const tab = document.getElementById(id);
+    tab.classList.add("active");
+  }
+}
+
+Tabs.addEventListener("click", handleTabs);
