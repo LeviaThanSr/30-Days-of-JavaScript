43d0edb40312468632fafa92ee92cdde2da780cc
LeviaThanSr <omarsarour@gmail.com>
2021-03-26 22:55:59 +0100

    Update App

--- Day 29/Popup Card/app.js
+++ Day 29/Popup Card/app.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+const container = document.querySelector(".container");
+const buttons = document.querySelectorAll(".btn");
+const popup = document.querySelector(".popup");
+
+function togglePopup(e) {
+  container.classList.toggle("blur");
+  popup.classList.toggle("active");
+  console.log(e);
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", togglePopup);
+});
